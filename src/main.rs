497680cdd8e98377506b765cd4a07958fc2b2582
Eddy Petrișor <eddy.petrisor@gmail.com>
2020-03-06 22:57:09 +0200

    Add some tests for defines extern C and comments
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,36 @@ mod tests {
         let parse = ApiParser::parse(Rule::p_define, "#defineFoo\n");
         assert!(parse.is_err());
     }
+
+    #[test]
+    fn extern_c_is_ok() {
+        let parse = ApiParser::parse(Rule::extern_c, "extern \"C\" { \n");
+        assert!(parse.is_ok());
+    }
+
+    #[test]
+    fn define_foo_no_val_is_ok() {
+        let parse = ApiParser::parse(Rule::p_define, "#define FOO\n");
+        assert!(parse.is_ok());
+    }
+
+    #[test]
+    fn define_foo_with_val_is_ok() {
+        let parse = ApiParser::parse(Rule::p_define, "#define FOO ((123) + 4) \n");
+        assert!(parse.is_ok());
+    }
+
+    #[test]
+    fn c_style_comments_simple() {
+        let parse = ApiParser::parse(Rule::comment, "/* foo /* bar */ baz \n");
+        assert!(dbg!(parse).is_ok());
+    }
+
+    #[test]
+    fn cpp_style_comments_simple() {
+        let parse = ApiParser::parse(Rule::comment, "//* foo /* bar */ baz \n");
+        assert!(parse.is_ok());
+    }
 }
 
 fn main() {
